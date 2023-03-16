d17b2076d53a00934931cc9fd6e0ad0fcdc01a09
Ina-Verena.Ludwig <ina.verena.ludwig@gmail.com>
2023-03-16 16:10:29 +0100

    start local store

--- src/app/strength/strength.component.ts
+++ src/app/strength/strength.component.ts
@@ -14,19 +14,32 @@ type Post = { strength: Array<DeviceStrength>, stamina: Array<DeviceStamina> };
 
 export class StrengthComponent implements OnDestroy{
 
-  imgURL: string = "../../assets/img/bild.png";
+  imgURL: string = "../../assets/img/";
   strength: Array<DeviceStrength> = [];
+  active: string = "body" || "body";
 
   constructor(http: HttpClient){
     const post$: Observable<Post> = http.get<Post>('/assets/json/device.JSON');
 
-    this.subscribe = post$.subscribe((post)=>{
-      this.strength = post.strength;
+    this.subscribe = post$.subscribe((post)=>{     
+      localStorage.setItem('device', JSON.stringify({post}));
     });
+  
+    let device: {post: Post} = JSON.parse(localStorage.getItem('device') || '');
+    this.strength = device.post.strength;    
+  }
+
+  createURL(name: string): string{
+    return this.imgURL + name;
+  }
+
+  filterArray(name: string): Array<DeviceStrength>{
+    return this.strength.filter(item=> item.trained === name);
   }
 
   subscribe: Subscription;
   ngOnDestroy(): void {
     this.subscribe.unsubscribe;
   }
+
 }
